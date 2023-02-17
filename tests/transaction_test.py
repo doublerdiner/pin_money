a0aa1b700ebb30c51e09ee00ad9041d9736f5272
Chris Barclay <chris.barclay1@gmail.com>
2023-02-17 14:45:14 +0000

    Initial transaction tests completed

--- tests/transaction_test.py
+++ tests/transaction_test.py
@@ -1,6 +1,37 @@
 import unittest
 from models.transaction import Transaction
+from datetime import datetime
 
 
 class TestTransaction(unittest.TestCase):
-    pass
+    transaction_1 = Transaction("Cinema", 18.00, "17 02 2023")
+
+    def test_transaction_has_name(self):
+        self.assertEqual("Cinema", self.transaction_1.name)
+
+    def test_transaction_has_cost(self):
+        self.assertEqual(18.00, self.transaction_1.cost)
+
+    def test_transaction_has_date(self):
+        self.assertEqual(datetime.strptime("17 02 2023", "%d %m %Y"), self.transaction_1.date)
+
+    def test_transaction_has_monthly_recurring(self):
+        self.assertEqual(False, self.transaction_1.monthly_recurring)
+
+    def test_transaction_has_notes(self):
+        self.assertEqual(None, self.transaction_1.notes)
+
+    def test_transaction_has_id(self):
+        self.assertEqual(None, self.transaction_1.id)
+
+    def test_transaction_monthly_recurring_status_change__True(self):
+        self.transaction_1.change_monthly_recurring()
+        self.assertEqual(True, self.transaction_1.monthly_recurring)
+        self.transaction_1.change_monthly_recurring()
+
+    def test_transaction_monthly_recurring_status_change__False(self):
+        self.transaction_1.monthly_recurring = True
+        self.transaction_1.change_monthly_recurring()
+        self.assertEqual(False, self.transaction_1.monthly_recurring)
+
+# 17/02/23 - Above tests passed
