7ac0a37e7a5a60aee62f796722d01d5ef9cfa8db
Chris Barclay <chris.barclay1@gmail.com>
2023-02-17 17:15:59 +0000

    Set up SQL database

--- db/spending_tracker.sql
+++ db/spending_tracker.sql
@@ -3,30 +3,30 @@ DROP TABLE IF EXISTS vendors;
 DROP TABLE IF EXISTS categories;
 DROP TABLE IF EXISTS transactions;
 
-CREATE TABLE transactions(
+CREATE TABLE transactions (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
+    name VARCHAR(255),   
     cost DECIMAL(10,2), 
     date DATE,
     monthly_recurring BOOLEAN,
-    notes VARCHAR(510),
+    notes VARCHAR(510)
 );
 
-CREATE TABLE categories(
+CREATE TABLE categories (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
-    transaction INT NOT NULL REFERENCES transactions(id)
+    transaction INT NOT NULL REFERENCES transactions(id),
     deactivated BOOLEAN
 );
 
-CREATE TABLE vendors(
+CREATE TABLE vendors (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
-    transaction INT NOT NULL REFERENCES transactions(id)
+    transaction INT NOT NULL REFERENCES transactions(id),
     deactivated BOOLEAN
 );
 
-CREATE TABLE goals(
+CREATE TABLE goals (
     id SERIAL PRIMARY KEY, 
     monthly_take_home_pay DECIMAL(10,2), 
     savings_target DECIMAL(10,2), 
