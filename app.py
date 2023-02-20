9d0b07b820cc08a0beb784d585f11896a97db091
Chris Barclay <chris.barclay1@gmail.com>
2023-02-20 10:56:39 +0000

    added home page

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from controllers.goal_controller import goal_blueprint
 from controllers.transaction_controller import transaction_blueprint
 from controllers.vendor_controller import vendor_blueprint
 from controllers.account_controller import account_blueprint
+from controllers.home_controller import home_blueprint
 
 app = Flask(__name__)
 
@@ -12,10 +13,11 @@ app.register_blueprint(goal_blueprint)
 app.register_blueprint(transaction_blueprint)
 app.register_blueprint(vendor_blueprint)
 app.register_blueprint(account_blueprint)
+app.register_blueprint(home_blueprint)
 
 @app.route('/')
 def home():
-    return render_template('index.html', title = "Home")
+    return render_template('welcome.html', title = "Welcome")
 
 if __name__ == '__main__':
     app.run(debug=True, port=4999)
---
+++ controllers/home_controller.py
@@ -0,0 +1,29 @@
+from flask import Flask, Blueprint, render_template, redirect
+import datetime
+from repositories import transaction_repository, account_repository, goal_repository
+
+home_blueprint = Blueprint("home", __name__)
+
+# INDEX
+# DISPLAY HOME
+@home_blueprint.route('/home')
+def home():
+    # Date
+    today = datetime.datetime.now()
+    month = today.strftime("%B")
+    month_int = today.month
+    year = today.strftime("%Y")
+    # transactions
+    pin_money = transaction_repository.pin_money_transactions_for_this_month(month_int)
+    monthly_recurring = transaction_repository.monthly_recurring_transactions()
+    pin_money_total = transaction_repository.total_transactions(pin_money)
+    monthly_recurring_total = transaction_repository.total_transactions(monthly_recurring)
+    # take_home_pay
+    accounts = account_repository.select_all()
+    account = accounts[0]
+    take_home_pay = account.take_home_pay
+    # Nearest Goal
+    goals = goal_repository.select_all()
+    goal = goals[0]
+       
+    return render_template("index.html", title="Home", month=month, year=year, take_home_pay=take_home_pay, pin_money=pin_money, monthly_recurring=monthly_recurring, pin_money_total=pin_money_total, monthly_recurring_total=monthly_recurring_total, goal=goal)
