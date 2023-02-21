8364e260ce787fd80c14b9796d070a1bd25d75db
Chris Barclay <chris.barclay1@gmail.com>
2023-02-21 18:46:43 +0000

    Styled the base page

--- controllers/home_controller.py
+++ controllers/home_controller.py
@@ -1,7 +1,10 @@
-from flask import Flask, Blueprint, render_template, redirect
+from flask import Flask, Blueprint, render_template, redirect, request
 import datetime
 from repositories import transaction_repository, account_repository, goal_repository
 from models.goal import *
+from models.transaction import Transaction
+from models.category import Category
+from models.vendor import Vendor
 
 home_blueprint = Blueprint("home", __name__)
 
@@ -31,8 +34,16 @@ def home():
     available_money = take_home_pay - grand_total
     to_be_saved = goal.savings_target - goal.saved_so_far 
     goal.time_remaining()
-    time = goal.goal_comment()
-
-       
+    time = goal.goal_comment()   
     return render_template("index.html", title="Home", month=month, year=year, take_home_pay=take_home_pay, pin_money=pin_money, monthly_recurring=monthly_recurring, pin_money_total=pin_money_total, monthly_recurring_total=monthly_recurring_total, goal=goal, grand_total=grand_total, available_money=available_money, to_be_saved=to_be_saved, time=time)
 
+@home_blueprint.route("/home/<id>", methods=['POST'])
+def update_goal_home(id):
+    goal = goal_repository.select(id)
+    name = goal.name
+    savings_target = goal.savings_target
+    savings_time_frame = goal.savings_time_frame
+    saved_so_far = float(goal.saved_so_far) + float(request.form['saved_so_far'])
+    goal = Goal(name, savings_target, savings_time_frame, saved_so_far, id)
+    goal_repository.update(goal)
+    return redirect ("/home")
